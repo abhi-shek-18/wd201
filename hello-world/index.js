4e2599f66401cc88c94690788c89dd1d79ac1f54
abhi-shek-18 <abhishek.chakraborty2021@uem.edu.in>
2023-02-21 20:05:23 +0530

    Initial commit

---
+++ hello-world/index.js
@@ -0,0 +1,8 @@
+const hello = () => {
+    console.log("hello Node.js!");
+};
+
+hello();
+
+//REPL=Read Evaluate Print Loop
+//it reads an input,evaluates it, prints the result and starts the process again.
---
+++ hello-world/new.js
@@ -0,0 +1,90 @@
+//all the commands in cmd
+/*C: \Users\ abhis > node
+Welcome to Node.js v16 .17 .0.
+Type ".help"
+for more information. >
+    const printlog = () => {
+        ...console.log("Multiline function block");
+        ...
+    }
+undefined
+    >
+    printlog[Function: printlog] >
+    printlog()
+Multiline
+function block
+undefined
+    >
+    str = "hello this is string"
+'hello this is string' >
+str. //after this press double tab
+str.__proto__ str.hasOwnProperty
+str.isPrototypeOf str.propertyIsEnumerable
+str.toLocaleString
+
+str.anchor str.at
+str.big str.blink
+str.bold str.charAt
+str.charCodeAt str.codePointAt
+str.concat str.constructor
+str.endsWith str.fixed
+str.fontcolor str.fontsize
+str.includes str.indexOf
+str.italics str.lastIndexOf
+str.length str.link
+str.localeCompare str.match
+str.matchAll str.normalize
+str.padEnd str.padStart
+str.repeat str.replace
+str.replaceAll str.search
+str.slice str.small
+str.split str.startsWith
+str.strike str.sub
+str.substr str.substring
+str.sup str.toLocaleLowerCase
+str.toLocaleUpperCase str.toLowerCase
+str.toString str.toUpperCase
+str.trim str.trimEnd
+str.trimLeft str.trimRight
+str.trimStart str.valueOf
+
+    >
+    str.toUpperCase()
+'HELLO THIS IS STRING' >
+"John Doe"
+'John Doe' >
+`My name is : ${_}`
+'My name is : John Doe' //underscore character refers the last string 
+
+.exit  //command used to exit the repl 
+
+C:\Users\abhis>node
+Welcome to Node.js v16.17.0.
+Type ".help" for more information.
+> arr=[1,2,3]
+[ 1, 2, 3 ]
+> str="Hello World"
+'Hello World'
+> .save mysession
+Session saved to: mysession
+> .exit
+
+C:\Users\abhis>cat mysession
+'cat' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\abhis>type mysession
+arr=[1,2,3]
+str="Hello World"
+C:\Users\abhis>node
+Welcome to Node.js v16.17.0.
+Type ".help" for more information.
+> .load mysession   //it loads all the code 
+arr=[1,2,3]
+str="Hello World"
+'Hello World'
+
+> .exit
+
+C:\Users\abhis>type .node_repl_history //returns history of all codes written
+*/
